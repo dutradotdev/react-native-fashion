5497e47bb3cf45d409cc8d31400589f97cbba028
lucas <dtrxxl@outlook.com>
2020-11-22 22:20:05 -0300

    feature: onboarding component

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,33 +1,133 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import React, { useRef } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import Animated, { multiply } from "react-native-reanimated";
+import {
+  useValue,
+  interpolateColor,
+  onScrollEvent,
+} from "react-native-redash/lib/module/v1";
 
-import Slide from "./Slide";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Subslide from "./Subslide";
+
+const { width } = Dimensions.get("window");
+
+const BORDER_RADIUS = 75;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
   },
   slider: {
-    flex: 0.61,
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
   footer: {
     flex: 1,
   },
+  footerContent: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
 });
 
-const Onboarding = () => (
-  <View style={styles.container}>
-    <View style={styles.slider}>
-      <ScrollView>
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
-      </ScrollView>
+const slides = [
+  {
+    title: "Relaxed",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfits? Dont't worry! Find the best outfit here!",
+    color: "#BFEAF5",
+  },
+  {
+    title: "Playful",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of outfits ideas",
+    color: "#BEECC4",
+  },
+  {
+    title: "Excentric",
+    subtitle: "Your Style, Your Way",
+    description:
+      "Create your individual & unique style and look amazing everyday",
+    color: "#FFE4D9",
+  },
+  {
+    title: "Funky",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Descover the latest trends in fashion and explore your personality",
+    color: "#FFDDDD",
+  },
+];
+
+const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <Animated.View
+          style={[
+            styles.footerContent,
+            {
+              width: width * slides.length,
+              flex: 1,
+              transform: [
+                {
+                  translateX: multiply(x, -1),
+                },
+              ],
+            },
+          ]}
+        >
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              onPress={() => {
+                if (scroll.current) {
+                  scroll.current.getNode().scrollTo({
+                    x: width * (index + 1),
+                    animated: true,
+                  });
+                }
+              }}
+              last={index === slides.length - 1}
+              {...{ subtitle, description }}
+            />
+          ))}
+        </Animated.View>
+      </View>
     </View>
-    <View style={styles.footer} />
-  </View>
-);
+  );
+};
 
 export default Onboarding;
