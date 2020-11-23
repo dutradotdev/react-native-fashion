0416ad60eb14bba348ea9378764179dff142e619
lucas <dtrxxl@outlook.com>
2020-11-22 22:19:46 -0300

    feature: slide component

--- src/Authentication/Onboarding/Slide.tsx
+++ src/Authentication/Onboarding/Slide.tsx
@@ -1,19 +1,43 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 
+const { width, height } = Dimensions.get("window");
+export const SLIDE_HEIGHT = 0.61 * height;
 interface SlideProps {
-  label: string;
+  title: string;
   right?: boolean;
 }
 
-const Slide = ({ label, right }: SlideProps) => {
+const Slide = ({ title, right }: SlideProps) => {
+  const transform = [
+    { translateY: (SLIDE_HEIGHT - 100) / 2 },
+    { translateX: right ? width / 2 - 50 : -width / 2 + 50 },
+    { rotate: right ? "-90deg" : "90deg" },
+  ];
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={styles.container}>
+      <View style={[styles.titleContainer, { transform }]}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
     </View>
   );
 };
 
 export default Slide;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width,
+  },
+  title: {
+    fontSize: 80,
+    lineHeight: 80,
+    fontFamily: "SFProText-Bold",
+    color: "white",
+  },
+  titleContainer: {
+    textAlign: "center",
+    height: 100,
+    justifyContent: "center",
+  },
+});
